Fixed bug in adaptive noise scaling + new weights of final agent

continuous_control/agent.py
@@ -109,6 +109,7 @@ class DDPGAgent:
         """
         states = torch.from_numpy(states).float().to(device)
         self.actor_local.eval()
+        self.actor_noised.eval()
         # Adaptive noise scaling from https://soeren-kirchner.medium.com/deep-deterministic-policy-gradient-ddpg-with-and-without-ornstein-uhlenbeck-process-e6d272adfc3
         with torch.no_grad():
             # get the action values from the noised actor for comparison
@@ -129,6 +130,9 @@ class DDPGAgent:
                     self.noise_scalar *= self.noise_scalar_decay
                 if distance <= self.noise_distance:
                     self.noise_scalar /= self.noise_scalar_decay
+
+                #  set the noised action as action
+                actions = actions_noised
 
         self.actor_local.train()
         if add_noise and self.noise_scalar is None:
@@ -165,7 +169,6 @@ class DDPGAgent:
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
         # Gradient clipping
-        # torch.nn.utils.clip_grad_norm(self.critic_local.parameters(), 1)
         self.critic_optimizer.step()
 
         # ---------------------------- update actor ---------------------------- #

continuous_control/train.py
@@ -106,9 +106,10 @@ def plot_scores(scores: List):
 
 def train_ddpg_complete(env: UnityEnvironment, brain_nm: str, agent: DDPGAgent, weights_path: str,
                         num_agents: int = 20, n_episodes: int = 2000,
-                        max_t: int = 1000, n_episodes_score: int = 100):
+                        max_t: int = 1000, n_episodes_score: int = 100, solving_score: int = 30):
     scores_deque = deque(maxlen=n_episodes_score)
     scores = []
+    solved_env = 0
     for i_episode in range(1, n_episodes + 1):
         env_info = env.reset(train_mode=True)[brain_nm]
         states = env_info.vector_observations
@@ -126,9 +127,13 @@ def train_ddpg_complete(env: UnityEnvironment, brain_nm: str, agent: DDPGAgent, 
                 break
         scores_deque.append(np.mean(score))
         scores.append(np.mean(score))
-        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_deque)), end="")
+        print('\rEpisode {}\t Score: {:.2f}\tAverage Score: {:.2f}'.format(i_episode,
+                                                                           np.mean(score),
+                                                                           np.mean(scores_deque)))
+        if not solved_env and i_episode >= n_episodes_score and np.mean(scores_deque) > solving_score:
+            print(f'Agent has been solved in {i_episode} episodes')
+            solved_env = 1
 
-        print('\rEpisode {}\t Score: {:.2f}'.format(i_episode, np.mean(score)))
         if i_episode % n_episodes_score == 0:
             torch.save(agent.actor_local.state_dict(), f'{weights_path}/checkpoint_actor.pth')
             torch.save(agent.critic_local.state_dict(), f'{weights_path}/checkpoint_critic.pth')